506c5f78e9de30de1b41d084d3119ffe3a068259
Guillermo Francisco García Acosta <74577801+TsintaLab@users.noreply.github.com>
2023-09-08 22:19:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('generarPDF').addEventListener('click', function () {
+document.getElementById('generar PDF').addEventListener('click', function () {
     const doc = new jsPDF();
     doc.text('Mi Página Web', 10, 10);
     doc.text(document.querySelector('body').innerText, 10, 20);
