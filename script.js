30cee1f9d5d01293b7a2caac1fdc2df782a75caa
Guillermo Francisco García Acosta <74577801+TsintaLab@users.noreply.github.com>
2023-09-08 22:40:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-document.getElementById('generarPDF').addEventListener('click', function () {
+
+document.getElementById('generarPDF).addEventListener('click', function () {
     const doc = new jsPDF();
-    doc.text('Formulario de Requerimientos de Sesión/Curso', 10, 10);
-    doc.text(document.querySelector('body').innerText, 10, 20);
-    doc.save('mi_pdf.pdf');
+
+    // Obtener los valores del formulario
+    const nombreCurso = document.getElementById('nombreCurso').value;
+    const nombreInstructor = document.getElementById('nombreInstructor').value;
+    const fechaCurso = document.getElementById('fechaCurso').value;
+    const lugarCurso = document.getElementById('lugarCurso').value;
+    const numParticipantes = document.getElementById('numParticipantes').value;
+
+    // Crear el contenido del PDF
+    const contenido = `
+        Nombre del curso: ${nombreCurso}
+        Nombre del Instructor: ${nombreInstructor}
+        Fecha del curso: ${fechaCurso}
+        Lugar: ${lugarCurso}
+        Número de participantes: ${numParticipantes}
+        
+        // Agregar aquí los indicadores y sus valores
+
+    `;
+
+    // Agregar el contenido al PDF
+    doc.text(contenido, 10, 10);
+
+    // Guardar el PDF como "formato_curso.pdf"
+    doc.save('formato_curso.pdf');
 });
